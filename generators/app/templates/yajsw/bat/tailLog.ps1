b43c0b57c4747fa08edf238e3ca606807d9565be
luiz.nazari <luiz.nazari@senior.com.br>
2019-01-22 10:27:46 -0200

    [REFACTOR][#FABPER-NO-TASK]: Corrigidos scripts tailLog .ps1 e .sh para trabalhar com o caminho absoluto do arquivo e não mais relativo ao diretório atual

--- generators/app/templates/yajsw/bat/tailLog.ps1
+++ generators/app/templates/yajsw/bat/tailLog.ps1
@@ -1,1 +1,12 @@
-Get-Content "service-wrapper/log/wrapper.log" -Wait -Tail 30
+function Get-WrapperRootDirectory {
+    return Split-Path -Path $PSScriptRoot -Parent
+}
+
+function Get-LogFilePath {
+    $WrapperDir = Get-WrapperRootDirectory
+    return "$($WrapperDir)\log\wrapper.log"
+}
+
+$LogFilePath = Get-LogFilePath
+
+Get-Content $LogFilePath -Wait -Tail 30
--- generators/app/templates/yajsw/bin/tailLog.sh
+++ generators/app/templates/yajsw/bin/tailLog.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 # -----------------------------------------------------------------------------
 
-tail -f "../log/wrapper.log"
+script_path=$(readlink -f "$0")
+wrapper_root_directory=$(dirname $(dirname "$script_path"))
+log_file_path="$wrapper_root_directory/log/wrapper.log"
+
+tail -f "$log_file_path"
